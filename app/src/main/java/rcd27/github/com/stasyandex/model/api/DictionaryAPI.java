a97f23d1b11f4133038d552bf7c69f0fcde7def6
rcd27 <redtom@yandex.ru>
2017-04-09 02:42:15 +0300

    добавил TODO

--- app/src/main/java/rcd27/github/com/stasyandex/model/api/DictionaryAPI.java
+++ app/src/main/java/rcd27/github/com/stasyandex/model/api/DictionaryAPI.java
@@ -7,6 +7,8 @@ import retrofit2.http.Query;
 import rx.Observable;
 
 public interface DictionaryAPI {
+    //TODO прикрутить возможность получать укороченную pos(часть речи)
+    //https://tech.yandex.ru/dictionary/doc/dg/reference/lookup-docpage/
     @GET("api/v1/dicservice.json/lookup")
     Observable<DicResultDTO> getDicResultFor(@Query("lang") String language,
                                              @Query("text") String text,
