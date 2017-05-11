7d43620310a06a446accd76ba90c0068f4f8d3c1
rcd27 <redtom@yandex.ru>
2017-05-11 21:45:08 +0300

    Запутался в стрелках, в задницу. Переделать.

--- app/src/main/java/rcd27/github/com/stasyandex/di/DictionaryComponent.java
+++ app/src/main/java/rcd27/github/com/stasyandex/di/DictionaryComponent.java
@@ -3,6 +3,7 @@ package rcd27.github.com.stasyandex.di;
 import dagger.Component;
 import rcd27.github.com.stasyandex.view.dictionary.DictionaryFragment;
 
+//TODO залить всё в Appcomponent: этот компонент и переводчика.
 @ApplicationScope
 @Component(modules = DictionaryModule.class)
 public interface DictionaryComponent {
