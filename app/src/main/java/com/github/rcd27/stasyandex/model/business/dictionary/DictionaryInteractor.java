65695f99a9bdbef64b2daee02369e094c3a83b1f
Zemlyakov Stas <redtom@yandex.ru>
2018-04-30 15:54:13 +0300

    DictionaryPresenter split.

--- app/src/main/java/com/github/rcd27/stasyandex/model/business/dictionary/DictionaryInteractor.java
+++ app/src/main/java/com/github/rcd27/stasyandex/model/business/dictionary/DictionaryInteractor.java
@@ -1,4 +1,20 @@
 package com.github.rcd27.stasyandex.model.business.dictionary;
 
+import com.github.rcd27.stasyandex.model.data.dictionary.*;
+import com.github.rcd27.stasyandex.presentation.dictionary.*;
+
+import io.reactivex.*;
+
+
 public class DictionaryInteractor {
+
+  private final DictionaryContract.Api api;
+
+  public DictionaryInteractor(DictionaryContract.Api api) {
+    this.api = api;
+  }
+
+  public Single<DicResult> getDicResultFor(String direction, String text, String language) {
+    return api.getDicResultFor(direction, text, language);
+  }
 }
--- app/src/main/java/com/github/rcd27/stasyandex/presentation/dictionary/DictionaryPresenter.java
+++ app/src/main/java/com/github/rcd27/stasyandex/presentation/dictionary/DictionaryPresenter.java
@@ -3,6 +3,7 @@ package com.github.rcd27.stasyandex.presentation.dictionary;
 
 import android.support.annotation.*;
 
+import com.github.rcd27.stasyandex.model.business.dictionary.*;
 import com.github.rcd27.stasyandex.presentation.*;
 
 import io.reactivex.android.schedulers.*;
@@ -11,20 +12,20 @@ import io.reactivex.disposables.*;
 public class DictionaryPresenter extends BasePresenter implements DictionaryContract.Presenter {
 
   private final DictionaryContract.View view;
-  private final DictionaryContract.Api api;
+  private final DictionaryInteractor interactor;
 
-  public DictionaryPresenter(DictionaryContract.View view, @NonNull DictionaryContract.Api api) {
+  public DictionaryPresenter(DictionaryContract.View view, DictionaryInteractor interactor) {
     this.view = view;
-    this.api = api;
+    this.interactor = interactor;
   }
 
   public void getDictionaryResponseFor(@NonNull String direction, @NonNull String text) {
-    addDisposable(getSubscriptionForDictionaryDefinition(direction, text));
+    addDisposable(getDictionary(direction, text));
   }
 
   @NonNull
-  private Disposable getSubscriptionForDictionaryDefinition(String direction, String text) {
-    return api.getDicResultFor(direction, text, "ru")
+  private Disposable getDictionary(String direction, String text) {
+    return interactor.getDicResultFor(direction, text, "ru")
         .observeOn(AndroidSchedulers.mainThread())
         .doOnError(throwable -> view.showEmpty())
         .subscribe(dicResult -> {
